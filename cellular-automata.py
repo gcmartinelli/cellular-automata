820f29e8a5faa7dac283c66bb4b405efdd877717
gcmartinelli <gcmartinelli@gmail.com>
2016-10-18 01:11:52 -0200

    Created initial state for coolness effect

--- cellular-automata.py
+++ cellular-automata.py
@@ -5,6 +5,7 @@ https://www.youtube.com/watch?v=bc-fVdbjAwk
 Automata rules used: http://atlas.wolfram.com/01/01/73/
 '''
 import random
+import time
 
 #Define cell states
 alive = u"\u2588"
@@ -17,14 +18,23 @@ def randomBinary():
     else:
         return dead
 
-def firstRow():
-    '''Initializes first row with random states'''
-    row = []
-    i = 0
-    while i < 100:
-        row.append(randomBinary())
-        i += 1
-    return row
+def firstRow(initial_state=[]):
+    '''Initializes first row with random states
+    if no initial state is given'''
+    if initial_state == []:
+        row = []
+        i = 0
+        while i <= 100:
+            row.append(randomBinary())
+            i += 1
+        return row
+    
+    if len(initial_state) != 101: #limit to cells per row
+        print('Initial state must have 101 cells')
+        return None
+    else:
+        return initial_state
+    
 
 def setStateRule73(cell_index, row):
     prev_ = cell_index - 1
@@ -52,8 +62,10 @@ def nextRow(row):
 
     return next_row
 
-def mainLoop(iterations=100):
-    first_row = firstRow()
+def mainLoop(iterations=1000):
+    initial_state = list(dead*50 + alive + dead*50) #One alive state
+    #initial_state = [] #Random state
+    first_row = firstRow(initial_state)
     next_row = nextRow(first_row)
 
     print(''.join(first_row))
@@ -61,6 +73,7 @@ def mainLoop(iterations=100):
     for _ in range(iterations):
         next_row = nextRow(next_row)
         print(''.join(next_row))
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     mainLoop()
